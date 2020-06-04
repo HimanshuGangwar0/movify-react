d46132284acaea7a4c7d384a632b2b2a01de221b
Himanshu <gangwar0145@gmail.com>
2020-06-05 01:36:53 +0530

    Updates

---
+++ precache-manifest.af0457380a4c52c4f050a832828a4a6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3446ef0e0dc6dd914307e5e58c072f0",
+    "url": "/movify-react/index.html"
+  },
+  {
+    "revision": "03b3c582282bc46d3459",
+    "url": "/movify-react/static/css/main.2591fc40.chunk.css"
+  },
+  {
+    "revision": "53e8f90da3c4a40e98e2",
+    "url": "/movify-react/static/js/2.0aa60f8b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movify-react/static/js/2.0aa60f8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03b3c582282bc46d3459",
+    "url": "/movify-react/static/js/main.ee178874.chunk.js"
+  },
+  {
+    "revision": "bd23552631f50cebfbd0",
+    "url": "/movify-react/static/js/runtime-main.02aca930.js"
+  }
+]);
